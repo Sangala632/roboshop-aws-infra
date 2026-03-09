62e62341fe5d325eaa69b5b55275892c1aa04637
sangala632 <sangala2003@gmail.com>
2026-03-09 10:36:37 +0530

    cdn vpn frontend some changes i made

--- 100-cdn/locals.tf
+++ 100-cdn/locals.tf
@@ -1,5 +1,6 @@
 locals {
     frontend_alb_certificate_arn = data.aws_ssm_parameter.frontend_alb_certificate_arn.value 
+    frontend_alb_dns = data.aws_ssm_parameter.frontend_alb_dns.value
     common_tags = {
         Project = var.project
         Environment = var.environment
--- 70-frontend-alb/parameters.tf
+++ 70-frontend-alb/parameters.tf
@@ -3,3 +3,9 @@ resource "aws_ssm_parameter" "frontend_alb_listener_arn" {
   type  = "String"
   value = aws_lb_listener.frontend_alb.arn
 }
+
+resource "aws_ssm_parameter" "frontend_alb_dns" {
+  name  = "/${var.project}/${var.environment}/frontend_alb_dns"
+  type  = "String"
+  value = module.frontend_alb.dns_name
+}
